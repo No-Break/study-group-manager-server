41bd3272d4d368e4dfd89c9fc0b4f5d39b790ecc
Heechul So <thrrkthrrk@gmail.com>
2021-07-17 23:19:50 +0900

    Add point history to csv file

--- src/main/kotlin/study/nobreak/studygroupmanagerserver/point/PointHistory.kt
+++ src/main/kotlin/study/nobreak/studygroupmanagerserver/point/PointHistory.kt
@@ -8,4 +8,16 @@ data class PointHistory(
     val point: Int,
     val reason: String,
     val createdDateTime: LocalDateTime
-)
+) {
+    companion object {
+        fun fromCsvLine(line: String): PointHistory {
+            return line.split(',').let {
+                PointHistory(it[0].toLong(), it[1].toLong(), it[2].toInt(), it[3], LocalDateTime.parse(it[4]))
+            }
+        }
+    }
+    
+    fun toCsvLine(): String {
+        return "$id,$userId,$point,$reason,$createdDateTime"
+    }
+}
