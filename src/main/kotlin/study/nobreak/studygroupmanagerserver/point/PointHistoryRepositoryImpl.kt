8c7575f9b78bcba8af172c082bda71b9cdd42298
Heechul So <thrrkthrrk@gmail.com>
2021-07-17 23:19:50 +0900

    Add point history to csv file

--- src/main/kotlin/study/nobreak/studygroupmanagerserver/point/PointHistoryRepositoryImpl.kt
+++ src/main/kotlin/study/nobreak/studygroupmanagerserver/point/PointHistoryRepositoryImpl.kt
@@ -1,17 +1,30 @@
 package study.nobreak.studygroupmanagerserver.point
 
+import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Repository
+import java.io.File
 import java.time.LocalDateTime
 
 @Repository
-class PointHistoryRepositoryImpl: PointHistoryRepository {
-    private val history: HashMap<Long, PointHistory> = hashMapOf()
+class PointHistoryRepositoryImpl(
+    resourceLoader: ResourceLoader
+): PointHistoryRepository {
+    private val file: File = resourceLoader.getResource("classpath:data/point-history.csv").file
+    private val data: LinkedHashMap<Long, PointHistory> = file.readLines().associate {
+        PointHistory.fromCsvLine(it)
+            .let { pointHistory -> pointHistory.id to pointHistory }
+    } as LinkedHashMap<Long, PointHistory>
     
     override fun add(userId: Long, point: Int, reason: String, createdDateTime: LocalDateTime): PointHistory {
         val nextId = this.nextId()
-        history[nextId] = PointHistory(nextId, userId, point, reason, createdDateTime)
-        return history[nextId]!!
+        data[nextId] = PointHistory(nextId, userId, point, reason, createdDateTime)
+        file.bufferedWriter().use { writer ->
+            data.forEach {
+                writer.write(it.value.toCsvLine())
+            }
+        }
+        return data[nextId]!!
     }
     
-    private fun nextId(): Long = history.keys.maxOrNull()?.plus(1) ?: 0L
+    private fun nextId(): Long = data.keys.maxOrNull()?.plus(1) ?: 0L
 }
